24a565352437faf4c23b37f65dcae29c48210fd3
lastDayIn2017 <lastDayIn2017@gmail.com>
2018-03-17 11:32:46 -0700

    set route for home controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
-  
+
   resources :student_drills
   resources :student_drill_groups
   resources :solutions
   resources :drills
   resources :drill_groups
   resources :users
+  resources :home, only: [:index, :leaderboard]
 
 end
