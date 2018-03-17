6da644ba0340130b081c6487eb73447bbac454a4
Kaysha George <33166759+kayshageorge@users.noreply.github.com>
2018-03-17 13:36:31 -0700

    added model dependencies (#11)

--- app/models/drill.rb
+++ app/models/drill.rb
@@ -1,6 +1,7 @@
 class Drill < ApplicationRecord
   belongs_to :drill_group
-  has_many :solutions
+
+  has_many :solutions, dependent: :destroy
 
   validates :question, presence: true, uniqueness: { scope: :drill_group, case_sensitive: false }
 
