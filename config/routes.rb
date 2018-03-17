3e9c52444b9f822aa60de775a1537808c3004ba9
lastDayIn2017 <lastDayIn2017@gmail.com>
2018-03-17 11:38:39 -0700

    fix routes for home controller

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
   resources :drills
   resources :drill_groups
   resources :users
-  resources :home, only: [:index, :leaderboard]
 
+  get('/', { to: 'home#index', as: :root })
+  get('/leaderboard', { to: 'home#leaderboard', as: :leaderboard })
+  
 end
