1060712de5674cbc738805ecb64745f463ff2292
Jay Jeong <jayjeong1988@gmail.com>
2018-03-17 11:09:33 -0700

    create validations for user model (#2)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,31 @@
 class User < ApplicationRecord
+# general
+
+  has_secure_password
+
 # admin
   has_many :drill_groups
-
 
 # students
   has_many :student_drill_groups
 
   has_many :student_drills
+
+# validations
+
+  validates :first_name, :last_name, presence: true
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+
+  validates :email,
+    presence: true,
+    uniqueness: true,
+    format: VALID_EMAIL_REGEX
+
+
+
+    def full_name
+    "#{first_name} #{last_name}".strip
+  end
+
 end
