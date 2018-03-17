4eaf6660bf05731936bb0207fabe1b898b4176fa
Metika Olanriksupak <metika.o@hotmail.com>
2018-03-17 11:29:27 -0700

    validate student group

--- app/models/student_drill.rb
+++ app/models/student_drill.rb
@@ -1,4 +1,7 @@
 class StudentDrill < ApplicationRecord
   belongs_to :user
   belongs_to :drill
+
+
+  
 end
--- app/models/student_drill_group.rb
+++ app/models/student_drill_group.rb
@@ -1,4 +1,17 @@
 class StudentDrillGroup < ApplicationRecord
   belongs_to :user
   belongs_to :drill_group
+
+  validates :user_id, uniqueness: { scope: :drill_group_id }
+
+  validates :points_rewarded, {
+    numericality: { greater_than_or_equal_to: 0 }
+  }
+
+  validates :score, {
+    numericality: { greater_than_or_equal_to: 0,
+    less_than_or_equal_to: 100
+    }
+  }
+
 end
