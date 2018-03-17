5dab6f3ccf3cc849c55d983a180aea76f7456488
lastDayIn2017 <lastDayIn2017@gmail.com>
2018-03-17 12:54:36 -0700

    fix route for users

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  
+
   resources :student_drills
   resources :student_drill_groups
   resources :solutions
   resources :drills
   resources :drill_groups
-  resources :users
+  resources :users, except: [:destroy]
 
 end
