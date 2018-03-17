98e238861a39369912c83d0c117dfa4f300da14a
Jay Jeong <jayjeong1988@gmail.com>
2018-03-17 12:04:38 -0700

    create home controller and view files for index and leader board (#4)
    * create home controller and view files for index and leader board
    
    * fix leader_board to leaderboard put view files into home directory
    
    * set route for home controller
    
    * fix routes for home controller

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,9 @@ Rails.application.routes.draw do
   resources :drills
   resources :drill_groups
   resources :users
-
   resource :session, only: [:new, :create, :delete]
+
+  get('/', { to: 'home#index', as: :root })
+  get('/leaderboard', { to: 'home#leaderboard', as: :leaderboard })
+   
 end
