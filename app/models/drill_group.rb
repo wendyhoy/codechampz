5bded7a73f66a4782cad7b108dbcf5497afb3e2c
lastDayIn2017 <lastDayIn2017@gmail.com>
2018-03-18 00:51:35 -0700

    create drill_groups#index page for non_admin users. create add button which creates new student_drill_group

--- app/models/drill_group.rb
+++ app/models/drill_group.rb
@@ -1,6 +1,6 @@
 class DrillGroup < ApplicationRecord
   belongs_to :user
-  
+
   has_many :drills, dependent: :destroy
 
   has_many :students, through: :student_drill_groups, source: :user
@@ -8,5 +8,17 @@ class DrillGroup < ApplicationRecord
   validates :name, presence: true, length: { in: 8..40 }, uniqueness: { case_sensitive: false }
   validates :description, presence: true, length: { minimum: 30 }
   validates :max_points, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
+
+  def friendly_level
+
+    if level == 1
+      'Beginner'
+    elsif level == 2
+      'Intermediate'
+    elsif level == 3
+      'Advanced'
+    end
+  end
 
 end
