782a07e24d3bf4495689a51e03ec38aaf8407638
wendyhoy <wendy.hoy@gmail.com>
2018-03-18 11:14:53 -0700

    Add times taken to student drill groups and save only highest points/score

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180317191849) do
+ActiveRecord::Schema.define(version: 20180318180339) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -50,6 +50,7 @@ ActiveRecord::Schema.define(version: 20180317191849) do
     t.integer "score", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "times_taken", default: 0
     t.index ["drill_group_id"], name: "index_student_drill_groups_on_drill_group_id"
     t.index ["user_id"], name: "index_student_drill_groups_on_user_id"
   end
