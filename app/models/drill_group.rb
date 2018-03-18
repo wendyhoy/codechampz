baea82e7b1647723bd1c16c10eb01b04eb6ea066
Metika Olanriksupak <metika.o@hotmail.com>
2018-03-18 16:00:09 -0700

    Add Actual Questions and Solutions

--- app/models/drill_group.rb
+++ app/models/drill_group.rb
@@ -5,9 +5,25 @@ class DrillGroup < ApplicationRecord
 
   has_many :students, through: :student_drill_groups, source: :user
 
-  validates :name, presence: true, length: { in: 8..40 }, uniqueness: { case_sensitive: false }
-  validates :description, presence: true, length: { minimum: 30 }
-  validates :max_points, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :level,
+    numericality: {
+      only_integer: true,
+      greater_than_or_equal_to: 1,
+      less_than_or_equal_to: 3
+    }
+
+  validates :name,
+    presence: true,
+    length: { in: 8..40 },
+    uniqueness: { case_sensitive: false, scope: :level }
+
+  validates :description,
+    presence: true,
+    length: { minimum: 30 }
+
+  validates :max_points,
+    presence: true,
+    numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
 
   def friendly_level
